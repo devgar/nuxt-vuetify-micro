74266cf6093c6ea97782f2b8385e9c05110d1de5
Edgar Albalate <dev.gardo@gmail.com>
2018-02-13 16:40:34 +0100

    Some style modifications in default laout

--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -1,20 +1,12 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
-    >
+      :mini-variant.sync="miniVariant" :clipped="clipped"
+      v-model="drawer" fixed app >
       <v-list>
-        <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
-          v-for="(item, i) in items"
-          exact
-        >
+        <v-list-tile router exact
+          :to="item.to" :key="i"
+          v-for="(item, i) in items" >
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
@@ -25,45 +17,30 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed app :clipped-left="clipped">
+      <!-- // Disabled layout switchers
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>web</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon @click.stop="fixed = !fixed" >
         <v-icon>remove</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+      <!-- // Disabled right layout switcher
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer" >
         <v-icon>menu</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
+    <v-navigation-drawer temporary :right="right" v-model="rightDrawer" fixed >
       <v-list>
         <v-list-tile @click.native="right = !right">
           <v-list-tile-action>
