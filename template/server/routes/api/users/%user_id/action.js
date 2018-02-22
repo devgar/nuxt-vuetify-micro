3d0f4b2f19a94268dbdd0705de9920b84d3d08d8
Edgar Albalate Ibañez <dev.gardo@gmail.com>
2018-02-22 11:22:02 +0100

    Update action.js

--- template/server/routes/api/users/%user_id/action.js
+++ template/server/routes/api/users/%user_id/action.js
@@ -1,6 +1,15 @@
+const {createError} = require('micro')
+const parse = require('micro-body')
 const {GET} = require('.')
 
-exports.GET = async (req) => {
-  var {name} = await GET(req)
+exports.GET = (req) => {
+  let {name} = GET(req)
   return {message: `${name} has done an action`}
+}
+
+exports.PUT = async (req) => {
+  let {name} = GET(req)
+  let {action} = await parse(req)
+  if (!action) throw createError(400, 'action not present')
+  return {message: `${name} has done an action: ${action}`}
 }
